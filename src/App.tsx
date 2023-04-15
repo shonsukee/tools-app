ed2638a877abb472e008e71678146f3e07a8a86c
okedey <97540020+okedey@users.noreply.github.com>
2023-04-15 09:11:39 +0900

    初期ページ

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+        Hello  Edit <code>src/App.tsx</code> and sve to reload.
+
+    </div>
+  );
+}
+
+export default App;
